0b88e5a9412fe31b55b96d9ddbbf86eebed0c4cd
Ptuxa <sdnaft@gmail.com>
2024-06-26 15:36:44 +0300

    Add: add admin user if there are no admins in the user table

--- src/main/java/by/modsen/practice/group11/PracticeModsenJavaApplication.java
+++ src/main/java/by/modsen/practice/group11/PracticeModsenJavaApplication.java
@@ -1,15 +1,26 @@
 package by.modsen.practice.group11;
 
+import by.modsen.practice.group11.config.InitConfiguration;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 @EnableCaching
+@RequiredArgsConstructor
 public class PracticeModsenJavaApplication {
+
+    private final InitConfiguration initConfiguration;
 
     public static void main(String[] args) {
         SpringApplication.run(PracticeModsenJavaApplication.class, args);
     }
 
+    @EventListener(ApplicationReadyEvent.class)
+    public void doSomethingAfterStartup() {
+        initConfiguration.init();
+    }
 }
